8f5755f8b035e69127e480257a3e4135d2716309
dionomusuko <ruby.school114@gmail.com>
2020-07-08 00:47:55 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 初回のみClone、以降はPullする
-if cd .github; then
-  git pull origin master;
+if cd .github ; then
+  git pull origin master
 else
-  git clone https://github.com/dionomusuko/go-docker-template.git;
+  git clone https://github.com/dionomusuko/go-docker-template.git
 fi
 
 # Makefileを実行
